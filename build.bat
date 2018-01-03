cc43eec4a93438a84a52fb87cd728884a9e450dc
John Brochue <john.brochue@ghielectronics.com>
2018-01-03 15:14:09 -0500

    Remove output dirs before build

--- build.bat
+++ build.bat
@@ -6,6 +6,14 @@ SET ScriptRoot=%~dp0
 
 PUSHD "%ScriptRoot%"
 
+RMDIR /s /q docs
+RMDIR /s /q obj
+
+MKDIR docs
+
+REM SET to supress newline at end of file
+ECHO | SET /p="docs.ghielectronics.com" > docs/CNAME
+
 docfx
 
 POPD
