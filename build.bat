1547fabb42967c0b50c687c7c4fa906125d333dd
John Brochue <johnbrochue@outlook.com>
2018-06-25 13:41:32 -0400

    Skip pdf build by default

--- build.bat
+++ build.bat
@@ -11,10 +11,15 @@ RMDIR /s /q obj
 
 MKDIR docs
 
+SET Target=%1
+
+IF "%Target%" == "" SET Target=build
+IF "%Target%" == "all" SET Target=
+
 REM SET to supress newline at end of file
 ECHO | SET /p="docs.ghielectronics.com" > docs/CNAME
 
-docfx
+docfx %Target%
 
 POPD
 
